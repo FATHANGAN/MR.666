34e18ed85e600ea35a42af7152f53f622b330dcd
FATHANGAN <115706744+FATHANGAN@users.noreply.github.com>
2022-10-18 16:05:50 +0700

    Update and rename README.md to BOT2.py

---
+++ BOT2.py
@@ -0,0 +1,21 @@
+
+import os,sys,time,requests,json,random
+
+
+
+def  logo_taekyung ():
+    coba :
+        os . sistem ( "xdg-buka https://youtube.com/channel/UCus7ckW0Nv6x5_AwBhDLI6Q")
+        waktu . tidur ( 3 )
+        os . sistem ( "jelas" )
+        cetak ( f"""
+
+
+
+Print : ("""
+
+
+       LU GAK SUBSCRIB YA
+
+
+""")
